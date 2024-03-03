edafcb1455ae9127cbac338b59a450a8ba2cb2f8
diswho <discover.who@gmail.com>
2024-03-03 17:22:14 +0700

    Lap18

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,22 +1,39 @@
+import { useState } from "react";
 import { Project } from "./Project";
 import ProjectCard from "./ProjectCard";
 import ProjectForm from "./ProjectForm";
 
 interface ProjectListProps {
   projects: Project[];
+  onSave: (project: Project) => void;
 }
-function ProjectList({ projects }: ProjectListProps) {
+
+function ProjectList({ projects, onSave }: ProjectListProps) {
+  const [projectBeingEdited, setProjectBeingEdited] = useState({});
+
   const handleEdit = (project: Project) => {
-    console.log(project);
+    // console.log(project);
+    setProjectBeingEdited(project);
   };
+
+  const cancelEditing = () => {
+    setProjectBeingEdited({});
+  };
+
   return (
     <div className="row">
       <ul className="row">
         {projects.map((project) => (
-          //   <li key={project.id}>{project.name}</li>
           <div key={project.id} className="cols-sm">
-            <ProjectCard project={project} onEdit={handleEdit}></ProjectCard>
-            <ProjectForm />
+            {project === projectBeingEdited ? (
+              <ProjectForm
+                project={project}
+                onSave={onSave}
+                onCancel={cancelEditing}
+              />
+            ) : (
+              <ProjectCard project={project} onEdit={handleEdit}></ProjectCard>
+            )}
           </div>
         ))}
       </ul>
--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,14 +1,82 @@
-import { MOCK_PROJECTS } from "./MockProjects";
+import { Fragment, useEffect, useState } from "react";
+import { Project } from "./Project";
 import ProjectList from "./ProjectList";
+import { projectAPI } from "./projectAPI";
+// import { MOCK_PROJECTS } from "./MockProjects";
 
 function ProjectsPage() {
-  // return <h1>Projects</h1>;
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const saveProject = (project: Project) => {
+    console.log("Saving project", project);
+    let updatedProjects = projects.map((p: Project) => {
+      return p.id === project.id ? project : p;
+    });
+    setProjects(updatedProjects);
+  };
+
+  const handleMoreClick = () => {
+    setCurrentPage((currentPage) => currentPage + 1);
+  };
+  useEffect(() => {
+    async function loadProjects() {
+      setLoading(true);
+      try {
+        // const data = await projectAPI.get(1);
+        const data = await projectAPI.get(currentPage);
+        // setError("");
+        if (currentPage === 1) {
+          setProjects(data);
+        } else {
+          setProjects((projects) => [...projects, ...data]);
+        }
+      } catch (e) {
+        if (e instanceof Error) {
+          setError(e.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadProjects();
+  }, [currentPage]);
   return (
-    <>
+    <Fragment>
       <h1>Project</h1>
-      {/* <pre>{JSON.stringify(MOCK_PROJECTS, null, "")}</pre> */}
-      <ProjectList projects={MOCK_PROJECTS} />
-    </>
+      {error && (
+        <div className="row">
+          <div className="card large error">
+            <section>
+              <p>
+                <span className="icon-alert inverse "></span>
+                {error}
+              </p>
+            </section>
+          </div>
+        </div>
+      )}
+      <ProjectList onSave={saveProject} projects={projects} />
+      {!loading && !error && (
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="button-group flued">
+              <button className="button default" onClick={handleMoreClick}>
+                More ...
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+      {loading && (
+        <div className="center-page">
+          <span className="spinner primary"></span>
+          <p>Loading...</p>
+        </div>
+      )}
+    </Fragment>
   );
 }
 export default ProjectsPage;
